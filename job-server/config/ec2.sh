096429f8c8fbf2bdb66e17d0bfc0bcfc05fb3ff2
David Durst <davidbdurst@gmail.com>
2015-09-10 16:25:51 -0400

    fixed bugs with commands to deploy to ec2 and to update ec2.conf with hostname of master server

--- job-server/config/ec2.sh
+++ job-server/config/ec2.sh
@@ -14,16 +14,16 @@ fi
 
 #run spark-ec2 to start ec2 cluster
 CLUSTER_NAME=medium1Slave
-EC2-DEPLOY="$bin"/../spark-1.5.0-bin-hadoop2.6/ec2/spark-ec2
-$EC2-DEPLOY --key-pair=$KEY_PAIR --identity-file=$SSH_KEY --region=us-east-1 --zone=us-east-1a --instance-type=m3.medium --slaves 1 launch $CLUSTER_NAME
+EC2DEPLOY="$bin"/../spark-1.5.0-bin-hadoop2.6/ec2/spark-ec2
+"$EC2DEPLOY" --key-pair=$KEY_PAIR --identity-file=$SSH_KEY --region=us-east-1 --zone=us-east-1a --instance-type=m3.medium --slaves 1 launch $CLUSTER_NAME
 #There is only 1 deploy host. However, the variable is plural as that is how Spark Job Server named it.
 #To minimize changes, I left the variable name alone.
-DEPLOY_HOSTS="$($EC2-DEPLOY get-master medium1Slave | tail -n1)"
+DEPLOY_HOSTS=$("$EC2DEPLOY" get-master $CLUSTER_NAME | tail -n1)
 
 #This line is a hack to edit the ec2.conf file so that the master option is correct. Since we are allowing Amazon to
 #dynamically allocate a url for the master node, we must update the configuration file in between cluster startup
 #and Job Server deployment
-sed -i 's/MASTERSERVERURL/$DEPLOY_HOSTS/g' "$bin"/ec2.conf
+sed -i -E "s/master = .*/master = \"spark:\/\/$DEPLOY_HOSTS:7077\"/g" "$bin"/../config/ec2.conf
 
 #open the port on the master for Spark Job Server to work
 aws ec2 authorize-security-group-ingress --group-name $CLUSTER_NAME --protocol tcp --port 8090 --cidr 0.0.0.0/0
