ca3d0ef193b78d7b1272bd1ed0a12a35bb71557c
David Durst <davidbdurst@gmail.com>
2015-09-15 17:01:49 -0400

    reworked config file and KMeansExample to better run on a cluster

--- bin/ec2_deploy.sh
+++ bin/ec2_deploy.sh
@@ -12,7 +12,7 @@ fi
 
 #run spark-ec2 to start ec2 cluster
 EC2DEPLOY="$bin"/../spark-1.5.0-bin-hadoop2.6/ec2/spark-ec2
-"$EC2DEPLOY" --key-pair=$KEY_PAIR --identity-file=$SSH_KEY --region=us-east-1 --zone=us-east-1a --instance-type=m3.medium --slaves 1 launch $CLUSTER_NAME
+"$EC2DEPLOY" --copy-aws-credentials --key-pair=$KEY_PAIR --hadoop-major-version=yarn --identity-file=$SSH_KEY --region=us-east-1 --zone=us-east-1a --instance-type=$INSTANCE_TYPE --slaves $NUM_SLAVES launch $CLUSTER_NAME
 #There is only 1 deploy host. However, the variable is plural as that is how Spark Job Server named it.
 #To minimize changes, I left the variable name alone.
 export DEPLOY_HOSTS=$("$EC2DEPLOY" get-master $CLUSTER_NAME | tail -n1)
@@ -23,10 +23,12 @@ export DEPLOY_HOSTS=$("$EC2DEPLOY" get-master $CLUSTER_NAME | tail -n1)
 cp "$bin"/../config/ec2.conf.template "$bin"/../config/ec2.conf
 sed -i -E "s/master = .*/master = \"spark:\/\/$DEPLOY_HOSTS:7077\"/g" "$bin"/../config/ec2.conf
 
-#open the port on the master for Spark Job Server to work
-aws ec2 authorize-security-group-ingress --group-name $CLUSTER_NAME-master --protocol tcp --port 8090 --cidr 0.0.0.0/0
+#open all ports so the master for Spark Job Server to work and you can see the results of your jobs
+aws ec2 authorize-security-group-ingress --group-name $CLUSTER_NAME-master --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
 
 cd "$bin"/..
 bin/server_deploy.sh ec2
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY"  root@$DEPLOY_HOSTS "(echo 'export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID' >> spark/conf/spark-env.sh)"
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY"  root@$DEPLOY_HOSTS "(echo 'export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY' >> spark/conf/spark-env.sh)"
 ssh -o StrictHostKeyChecking=no -i "$SSH_KEY"  root@$DEPLOY_HOSTS "(cd job-server; nohup ./server_start.sh < /dev/null &> /dev/null &)"
 echo "The Job Server is listening at $DEPLOY_HOSTS:8090"
--- job-server-extras/src/spark.jobserver/KMeansExample.scala
+++ job-server-extras/src/spark.jobserver/KMeansExample.scala
@@ -3,12 +3,9 @@ package spark.jobserver
 import com.typesafe.config.Config
 import org.apache.spark._
 import org.apache.spark.ml.clustering.KMeans
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SQLContext, Row}
-import org.apache.spark.ml.feature.StandardScaler
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.DataFrameHolder
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+
 /**
  * A Spark job example that implements the SparkJob trait and can be submitted to the job server.
  *
@@ -17,9 +14,10 @@ import org.apache.spark.mllib.linalg.Vector
  *
  * validate() returns SparkJobInvalid if there is no input.string
  */
-object KMeansExample extends SparkJob with NamedRddSupport  {
+object KMeansExample extends SparkJob with NamedRddSupport {
   val NUM_ITERATIONS = 100
   val K = 7
+
   /**
    * Assume that the job succeeds
    * @param sc
@@ -29,18 +27,17 @@ object KMeansExample extends SparkJob with NamedRddSupport  {
   override def validate(sc: SparkContext, config: Config): SparkJobValidation = SparkJobValid
 
   override def runJob(sc: SparkContext, config: Config): (Array[String], Array[String]) = {
+    //load the hadoop configuration file, since the job server doesn't seem to do it
     val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
 
     //try to load namedRDD
     val cacheReturnOption = namedRdds.get[Row]("kmeans")
-    if(cacheReturnOption.isDefined) {
+    if (cacheReturnOption.isDefined) {
       //we can't use the toDF function since a row isn't of the Product interface, so the conversion fails
       val cacheReturn = cacheReturnOption.get.cache()
       val cacheReturnDF = sqlContext.createDataFrame(cacheReturn, cacheReturn.take(1)(0).schema)
       sampleAndReturn(cacheReturnDF)
     }
-
     else {
       val data = sqlContext.read.parquet("s3n://us-east-1.elasticmapreduce.samples/flightdata/input")
       val intCols = data.dtypes.filter(_._2 == "IntegerType").map(_._1).map(data.col(_))
@@ -49,27 +46,25 @@ object KMeansExample extends SparkJob with NamedRddSupport  {
       val noNADF = intDF.na.fill(0).cache()
 
       //since kmeans needs each row to have a feature vector, must assemble all columns into a vector
-      val assembler = new VectorAssembler().setInputCols(intCols.map(_.toString())).setOutputCol("features")
-      val featuresDF = assembler.transform(noNADF)
-
-      val kmeans = new KMeans()
-      kmeans.setK(K)
-      kmeans.setMaxIter(NUM_ITERATIONS)
-      kmeans.setFeaturesCol("Features")
-      kmeans.setPredictionCol("Output")
+      val assembler = new VectorAssembler().setInputCols(intCols.map(_.toString())).setOutputCol("Features")
+      val featuresDF = assembler.transform(noNADF))
 
       val stdScalerObject = new StandardScaler()
       stdScalerObject.setWithMean(false)
       stdScalerObject.setWithStd(true)
       stdScalerObject.setInputCol("Features")
 
-      lazy val scaler = stdScalerObject.fit(featuresDF)
+      val scaler = stdScalerObject.fit(featuresDF)
 
-      lazy val scaledData = scaler.transform(featuresDF)
+      val scaledData = scaler.transform(featuresDF)
 
+      val kmeans = new KMeans()
+      kmeans.setK(K)
+      kmeans.setMaxIter(NUM_ITERATIONS)
+      kmeans.setFeaturesCol("Features")
+      kmeans.setPredictionCol("Output")
       val dataWithPredictions = kmeans.fit(scaledData).transform(scaledData).cache()
       namedRdds.update("kmeans", dataWithPredictions.rdd.map(Tuple1(_)))
-
       sampleAndReturn(dataWithPredictions)
     }
 
@@ -78,7 +73,7 @@ object KMeansExample extends SparkJob with NamedRddSupport  {
 
   def sampleAndReturn(dataWithPredictions: DataFrame): (Array[String], Array[String]) = {
     //take 1000 points
-    val sample = dataWithPredictions.drop("Features").sample(false, 1000/dataWithPredictions.count())
+    val sample = dataWithPredictions.drop("Features").sample(false, 1000 / dataWithPredictions.count())
     (sample.columns, sample.toJSON.collect())
   }
 }
